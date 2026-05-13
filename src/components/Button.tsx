6fcf31f14d1c7624d1998729da1ae7f37fdbd3b3
Bemnet kassa Bekele <bemnetkassa79@gmail.com>
2026-05-13 10:48:47 +0300

    starting the project

---
+++ src/components/Button.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+// Let's just create a simple Button without radix for now, or just use standard Tailwind.
